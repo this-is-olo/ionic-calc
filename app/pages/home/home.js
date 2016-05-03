1b1bc4d1c7e337d4651c38fc2207ffc84d9efa28
Alexandru Ologu <alexandru.ologu@stud.acs.pub.ro>
2016-05-03 14:16:56 +0300

    made first build

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -6,6 +6,47 @@ import {Page} from 'ionic-angular';
 
 export class Home {
     constructor() {
-        this.user = "Alex";
+        this.var1 = 0;
+        this.var2 = 0;
+        this.res = 0;
+    }
+
+    //execute(val) {
+    //    if (this.op.length == 0){
+    //        this.res = val;
+    //        return 0;
+    //    }
+    //    let crtOp = this.op.pop();
+    //    console.log(crtOp);
+    //    console.log(val);
+    //    switch (crtOp) {
+    //        case "+" :
+    //            this.res = (parseInt(this.res) || 0) + val;
+    //            break;
+    //        case "-" :
+    //            this.res = (parseInt(this.res) || 0) + val;
+    //            break;
+    //        case "/" :
+    //            this.res = (parseInt(this.res) || 0) / val;
+    //            break;
+    //        case "*" :
+    //            this.res = (parseInt(this.res) || 0) * val;
+    //            break;
+    //    }
+    //}
+    //
+    //onKey(event) {
+    //    let key = event.key || String.fromCharCode(event.keyCode);
+    //    let val = parseInt(this.eq);
+    //    if (isNaN(parseInt(key))) {
+    //        this.execute(this.num);
+    //        this.num = val;
+    //        this.op.push(key);
+    //        this.eq = "";
+    //    }
+    //}
+
+    compute() {
+        this.res = parseFloat(this.var1) + parseFloat(this.var2);
     }
 }
